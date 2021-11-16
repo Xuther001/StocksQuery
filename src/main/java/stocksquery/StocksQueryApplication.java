3f5cc9bdc16a90e67f32a5e2dcb7b5b5ff1fd560
Bill <billtran@berkeley.edu>
2021-11-16 11:21:12 -0800

    Version2 removed CommandLineRunner. Added feature to search for stocks

--- src/main/java/stocksquery/StocksQueryApplication.java
+++ src/main/java/stocksquery/StocksQueryApplication.java
@@ -1,10 +1,8 @@
 package stocksquery;
 
 import java.util.List;
+import java.util.Scanner;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -17,31 +15,32 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class StocksQueryApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(StocksQueryApplication.class);
-
+	private static String stockSymbol;
+	
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
 		SpringApplication.run(StocksQueryApplication.class, args);
+		
+		while (!false) {
+		System.out.println("Please enter a stock symbol");
+		stockSymbol = input.next();
+		RestTemplate template = new RestTemplate();;
+		Data data = template.getForObject(
+				"https://finnhub.io/api/v1/stock/insider-transactions?symbol=" + stockSymbol + "&token=c683tuqad3iagio36uj0", Data.class);
+		
+		ResponseEntity<List<String>> peersResponse = template.exchange("https://finnhub.io/api/v1/stock/peers?symbol=CRM&token=c683tuqad3iagio36uj0",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
+        });
+		
+		List<String> peers = peersResponse.getBody();
+		System.out.println(peers);
+		System.out.println(data);
+		}
 	}
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Data data = restTemplate.getForObject(
-					"https://finnhub.io/api/v1/stock/insider-transactions?symbol=CRM&token=c683tuqad3iagio36uj0", Data.class);
-			log.info(data.toString());
-			System.out.println(data);
-			
-			ResponseEntity<List<String>> peersResponse =
-			        restTemplate.exchange("https://finnhub.io/api/v1/stock/peers?symbol=CRM&token=c683tuqad3iagio36uj0",
-			                    HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
-			            });
-			List<String> peers = peersResponse.getBody();
-			System.out.println(peers);
-		};
 	}
 }
